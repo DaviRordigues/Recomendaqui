0eef1d93679639d537e90887e3f95314ceb0377a
DaviRordigues <davirodriguesbezerra@gmail.com>
2024-04-29 15:59:28 -0300

    quinto commit

---
+++ site/script.js
@@ -0,0 +1,30 @@
+function criarConta() {
+    Swal.fire({
+        icon: 'success',
+        title: 'Conta Criada',
+        text: 'Sua conta foi criada com sucesso!',
+    });
+}
+
+function exibirInformacoes() {
+    Swal.fire({
+        icon: 'info',
+        title: 'Informações',
+        text: 'Você será redirecionado para as informações em breve.',
+    });
+}
+
+function entrar() {
+    Swal.fire({
+        icon: 'success',
+        title: 'Entrar no Sistema',
+        text: 'Você está prestes a entrar no sistema.',
+    });
+}
+function exibirPopUp() {
+    Swal.fire({
+        icon: 'success',
+        title: 'Recomende',
+        text: 'Você sera redirecionado para a aba de recomendaçao',
+    });
+}
